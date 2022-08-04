964e4a8102b97fe545604b93a9ec7f05a50c8139
husanbek <husanbek4030m@gmail.com>
2022-08-04 16:35:28 +0500

    finished four zadacha

--- SwiftUIFout/Screens/TaskFour.swift
+++ SwiftUIFout/Screens/TaskFour.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct TaskFour: View {
+    @State private var showAlert = false
+    @State var showSheet = false
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        TabView {
+            Button(action: {
+                self.showAlert = true
+            }, label: {
+                Text("Show alert")
+            }).alert(isPresented: $showAlert, content: {
+                let title = "Logut"
+                let msg = "You have logout?"
+                
+                return Alert(title: Text(title), message: Text(msg), primaryButton: .destructive(Text("No")), secondaryButton: .default(Text("Yes")))
+            })  .font(.title)
+                .tabItem({
+                    Image(systemName: "clock")
+                    Text("Alert")
+                })
+                .tag(0)
+            
+            
+            Button(action: {
+                self.showSheet = true
+            }, label: {
+                Text("Show confirm")
+            }).actionSheet(isPresented: $showSheet, content: {
+                let title = "Action"
+                let msg = "Description"
+                
+                return ActionSheet(title: Text(title), message: Text(msg), buttons: [
+                    .default(Text("Yes")),
+                    .destructive(Text("No"))
+                ])
+            })
+                .font(.title)
+                .tabItem({
+                    Image(systemName: "bell")
+                    Text("Confirm")
+                })
+                .tag(1)
+            
+        }
+        
     }
 }
 
